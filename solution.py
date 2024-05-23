9304382cefd70f84ba01a7fece61b675dcc6144a
Kooreis <kooreis.official@gmail.com>
2024-05-23 16:12:30 -0500

    This update introduces the BSTtoCDLL class, which is used to convert a BST into a CDLL. The convert method is a recursive function that traverses the BST in an in-order fashion and creates a new node for the CDLL for each node it visits in the BST.

--- solution.py
+++ solution.py
@@ -1,4 +1,22 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = self.right = None
+class BSTtoCDLL:
+    def __init__(self):
+        self.head = None
+        self.prev = None
+
+    def convert(self, root):
+        if root is None:
+            return
+
+        self.convert(root.left)
+
+        node = Node(root.data)
+
+        if self.prev is None:
+            self.head = node
+        else:
+            self.prev.right = node
+            node.left = self.prev
+
+        self.prev = node
+
+        self.convert(root.right)
